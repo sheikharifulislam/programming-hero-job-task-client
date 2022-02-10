f0e77cfd9c70f262e691c42ff7eb0291d205fa37
sheikh ariful <sheikharifulislam770@gmail.com>
2022-02-10 07:02:34 +0600

    update index.html

--- src/pages/dashboard/dashboardHome/DashboardHome.js
+++ src/pages/dashboard/dashboardHome/DashboardHome.js
@@ -3,7 +3,7 @@ import React from 'react';
 const DashboardHome = () => {
     return (
         <div>
-            <h1>I am from DashboardHome</h1>
+            <h1 style={{textAlign: 'center'}}>This is dashboard main page. To see all types of information, please click on the link next to it</h1>
         </div>
     );
 };
